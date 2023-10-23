78889c78f4e9f1686b7726d71d73ba0f10a53232
Jaylee Baker <jbaker50@murraystate.edu>
2023-10-23 12:25:33 -0500

    basic login screen

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./components/Header";
 import Body from "./components/Body";
+import Login from "./components/Login";
 
 function App() {
+  const [isLoggedIn, setLoggedIn] = useState(false);
   return (
-    <div className=" overflow-hidden  overflow-x-scroll">
-        <Header/>
-        <Body />
-    </div>
+    <>
+      {!isLoggedIn ? (<Login stateFunction={setLoggedIn} />):(
+        <div className=" overflow-hidden  overflow-x-scroll">
+          <Header/>
+          <Body />
+        </div>
+      )}
+    </>
+    
   );
 }
 
---
+++ src/components/Login.js
@@ -0,0 +1,46 @@
+import React, { useState } from "react";
+import '../login.css';
+
+
+function Login({stateFunction}) {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleLogin = () => {
+
+            console.log('clicked');
+            stateFunction(true);
+        };
+
+    return (
+        <div className="title-page">
+
+            <div className="title-container">
+            <h1 className="title">Taskmaster</h1>
+            </div>
+        
+            <div className="login-container">
+                <h2>Login</h2>
+                <form>
+                    <label>Email:
+                        <input 
+                        type="text"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                    />
+                    </label>
+                    <label>Password:
+                        <input
+                        type="password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                    />
+                    </label>
+                    <button onClick={handleLogin}>Login</button>
+                </form>
+            </div>
+        </div>
+    );
+}
+
+export default Login;
