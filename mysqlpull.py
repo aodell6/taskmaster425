ded2c52e28dd32b00aa2d2163c9fba1e932d30cb
Nathan Borchelt <nathan.d.borchelt@gmail.com>
2023-10-31 14:01:11 -0500

    db connect with python

---
+++ mysqlpull.py
@@ -0,0 +1,38 @@
+import pymysql
+
+timeout = 10
+connection = pymysql.connect(
+    charset="utf8mb4",
+    connect_timeout=timeout,
+    cursorclass=pymysql.cursors.DictCursor,
+    db="defaultdb",
+    host="mysql-d51f41f-nathan-a42e.a.aivencloud.com",
+    password="AVNS_3kWPcqsUyzZ8o_Ylw1a",
+    read_timeout=timeout,
+    port=21984,
+    user="avnadmin",
+    write_timeout=timeout,
+)
+
+try:
+    cursor = connection.cursor()
+    cursor.execute("USE TaslDatabase")
+    cursor.execute("""CREATE TABLE `TaskDatabase` (
+	`TaskID` INT unsigned NOT NULL AUTO_INCREMENT,
+	`UserID` INT NOT NULL,
+	`TaskTitle` VARCHAR(255) NOT NULL,
+	`TaskDescription` TEXT  NOT NULL,
+	`TaskType` TINYINT unsigned NOT NULL DEFAULT '0',
+	`DueDate` DATE NOT NULL,
+	PRIMARY KEY (`TaskID`));""")
+
+
+    cursor.execute('INSERT INTO TaskDatabase ("UserID", "TaskTitle", "TaskDescription", "TaskType", "DueDate") VALUES (1, "test title","test desc",1, "2023-10-31")')
+    print(cursor.fetchall())
+
+except Exception as e:
+    print("hit exception")
+    print(e)
+
+finally:
+    connection.close()
